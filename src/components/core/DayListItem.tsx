ed88c13917a29eec015404d08923def5f181e857
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-12 14:28:13 +0100

    advent calender list

---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from "react-native";
+
+type DayListItem = {
+  day: number;
+};
+
+function DayListItem({ day }: DayListItem) {
+  return (
+    <View style={styles.box}>
+      <Text style={styles.text}>{day}</Text>
+    </View>
+  );
+}
+
+export default DayListItem;
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#f9ede3",
+    // width: 300,
+    // height: 300,
+
+    flex: 1,
+    aspectRatio: 1,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9b4521",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#9b4521",
+    fontSize: 70,
+  },
+});
