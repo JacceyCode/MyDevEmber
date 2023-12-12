1e81919b82efc3ba2be5812a3e3f224878a3a884
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-12 17:42:41 +0100

    Stack navigator

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,8 +1,10 @@
+import { Stack } from "expo-router";
 import { View, Text } from "react-native";
 
 function DayDetailsScreen() {
   return (
     <View>
+      <Stack.Screen options={{ title: "Day 1" }} />
       <Text>Day Details Screen</Text>
     </View>
   );
