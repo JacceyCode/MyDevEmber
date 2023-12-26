07fe5a4e9e5821253a436a3b20721d46ca17edd3
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-26 23:28:58 +0100

    installed markdowwn display dependency

--- src/app/(days)/day3/editor.tsx
+++ src/app/(days)/day3/editor.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import Markdown from "react-native-markdown-display";
+
+const copy = `# h1 Heading 8-)
+
+**This is some bold text!**
+
+This is normal text
+`;
 
 function EditorScreen() {
   return (
     <View>
-      <Text>Editor Screen</Text>
+      <Markdown>{copy}</Markdown>
     </View>
   );
 }
 
 export default EditorScreen;
+
+const styles = StyleSheet.create({});
